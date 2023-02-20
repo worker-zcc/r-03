7d3148c8a1996797562aef459ec345f4d788a1f5
zcc <zhangcongcong.work@foxmail.com>
2023-02-20 10:59:54 +0800

    修改：跨域测试用例

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 /*
- * @Desc: 
+ * @Desc:
  * @Author: zcc
  * @LastEditors: zcc
  * @Date: 2023-02-14 10:06:25
- * @LastEditTime: 2023-02-14 15:44:26
+ * @LastEditTime: 2023-02-20 10:59:36
  */
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <div>111</div>
   </React.StrictMode>
 );
+fetch("/jian/subscriptions/recommended_collections")
+  .then((response) => response.json())
+  .then((res) => {
+    console.log(res);
+  });
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,18 +1,18 @@
 /*
- * @Desc:
+ * @Desc: 代理配置
  * @Author: zcc
  * @LastEditors: zcc
  * @Date: 2023-02-20 10:20:43
- * @LastEditTime: 2023-02-20 10:48:31
+ * @LastEditTime: 2023-02-20 10:53:48
  */
 const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware("/api", {
-      target: "", //代理地址
+    createProxyMiddleware("/jian", {
+      target: "https://www.jianshu.com/asimov", //代理地址
       changeOrigin: true,
       ws: true,
-      pathRewrite: { "^/api": "" },
+      pathRewrite: { "^/jian": "" },
     })
   );
 };
